06516c9a0d12c05e7499bdef79e3e77fd7a88741
khushi-prajapati30 <khushikprajapati9053@gmail.com>
2026-01-19 15:07:53 +0530

    first api

--- day2project/src/main/java/com/example/day2project/controller/home.java
+++ day2project/src/main/java/com/example/day2project/controller/home.java
@@ -1,14 +1,18 @@
 package com.example.day2project.controller;
 
+import com.example.day2project.models.studentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class home {
 
-    @GetMapping("/about")
-    public String about(){
-        return "This is about page";
+    @GetMapping("/")
+    public studentModel getStudent(){
+        studentModel student = new studentModel(1, "Khushi Prajapati", "khushikprajapati9053@gmail.com");
+        //we can also do it using set
+        return student;
     }
+
 
 }
--- day2project/src/main/java/com/example/day2project/models/studentModel.java
+++ day2project/src/main/java/com/example/day2project/models/studentModel.java
@@ -28,4 +28,14 @@ public class studentModel {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public studentModel(){
+    }
+
+    public studentModel(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
 }
